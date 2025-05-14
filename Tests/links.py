8dc28063797d34221b72cae90680d036831b6f0c
Georgiy Shestakov <gvshestakov@inbox.ru>
2025-05-14 22:35:38 +0300

    1. Наименования пакетов переведены в snake_case. 2. Тестовые данные перенесены в data.py. 3.URL перенесены в links.py и используется конкатенация. 4.Локаторы переписаны. 5.В tests_login.py добавлены шаги теста до assert. 6. Классы переименованы в CamelCase

---
+++ Tests/links.py
@@ -0,0 +1,5 @@
+class UrlLocators:
+    URL_MAIN = "https://qa-desk.stand.praktikum-services.ru"
+    URL_LOGIN = f"{URL_MAIN}/login"
+    URL_REGISTRATION = f"{URL_MAIN}/regiatration"
+    URL_PROFILE = f"{URL_MAIN}/profile"
